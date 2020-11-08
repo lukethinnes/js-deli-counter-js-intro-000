79d3457ef07c423545bea0462b37476b48725e68
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-08 22:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine) {
     else {
         var arrayWithIndexAndName = []
         for (let i = 0; i < katzDeliLine.length; i++) {
-            arrayWithIndexAndName.push(`${i + 1}. ${katzDeliLine[i]}`)
+            arrayWithIndexAndName.push(` ${i + 1}. ${katzDeliLine[i]}`)
             }
         return `The line is currently: ${arrayWithIndexAndName}`
     }
